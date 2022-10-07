7fc49231a5a2c334c0edf2d4faa709b705d01e3d
abhi3k <abhisheks71@yahoo.com>
2022-10-07 21:22:24 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Abhishek";
+        // name is a private member hence can not be accessed
+        obj.setName("Abhishek");
+        System.out.println(obj.getName());
+    }
 }
